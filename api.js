67be56117afab4e126fe9441bd6123f3ec498dbb
eatnug <wlq4568@gmail.com>
2019-09-05 00:58:42 +0900

    강의 목록 긁어와서 내가 트래킹 할 강의 마이페이지에 추가하기 까지 완성

--- api.js
+++ api.js
@@ -2,6 +2,8 @@ const axios = require('axios')
 const qs = require('qs')
 const cheerio = require('cheerio');
 
+const url = "https://wis.hufs.ac.kr/src08/jsp/lecture/LECTURE2020L.jsp"
+
 const data = qs.stringify({
     tab_lang: "K",
     ag_ledg_year: "2019",
@@ -9,85 +11,47 @@ const data = qs.stringify({
     ag_org_sect: "A",
     campus_sect: "H!",
     ag_crs_strct_cd: "A1CE1_H1",
-    cn: "A01731103",
+    cn: "A01733102",
 })
 
+
+const cn = "A01733102"
+
+/**
+ *  axios post로 데이터 받아오는 함수
+ * @param {*} url https://wis.hufs.ac.kr/src08/jsp/lecture/LECTURE2020L.jsp
+ * @param {*} data 포스트로 넘길 폼 데이터 qs stringify
+ */
+async function getData(data) {
+    console.log(data)
+    return await axios.post("https://wis.hufs.ac.kr/src08/jsp/lecture/LECTURE2020L.jsp", data, {'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'});
+}
+
+/**
+ * 자리 남았는지 알려주는 함수
+ * @param {*} res http response
+ * @param {*} CN course number
+ */
 const parseGetLeftSeat = async (res, CN) => {
     return new Promise((resolve, reject) => {
         const $ = cheerio.load(res.data);
         $("#premier1 > div > table > tbody > tr > td").each((i, e) => {
-            if (e.firstChild && e.firstChild.data == CN) {
+            if (e.firstChild && e.firstChild.data == CN && e.next) {
                 const tmp = e.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.firstChild.data.split('/')
-                resolve(Number(tmp[1].trim()) - Number(tmp[0].trim()))
+                resolve(Number(tmp[1].trim()) - Number(tmp[0].trim()) == 0 ? false : true)
             }
         })
     })
 }
 
-axios.post('https://wis.hufs.ac.kr/src08/jsp/lecture/LECTURE2020L.jsp', data, {
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-    })
-    .then(async (res) => {
-        const CN = qs.parse(data).cn
-        console.log((await parseGetLeftSeat(res, CN)))
-    })
-    .catch((error) => {
-        console.error(error)
-    })
+const noticeEmpty = async(data, CN, cb) => {
+    if(await parseGetLeftSeat(await getData(data), CN)) cb()
+    else {
+        setTimeout(async() => {
+            await noticeEmpty(data,CN,cb)
+        })
+    }
+}
 
-
-/**
-     * <tr>
-                    <td>1</td>
-                    
-                        <td>인문학공통</td>
-                    
-                    <td>1</td>
-                    <td>J11019101</td>
-                    <td class="align_left">
-                        
-                            <!-- onClick="javascript:go_syllabus('2019','3','A','J11019101')" -->
-                            <div 
-                                    onClick="javascript:lecture_open('2019','3','A','J11019101')"
-                                 >
-                                <font class="txt_navy">러시아.투르크.몽골의 사상과 문화<br>
-                                <font class='txt_gray8'>(Russian, Turkic, Mongolian Thoughts and Cultures)
-                            </div>
-                        
-					</td>
-					
-					<!-- syllabus -->
-                    <td>
-                            
-                                <a href="javascript:lecture_open('2019','3','A','J11019101')">
-                                <img src="/src08/images/common/ico_file.gif" border="0"></a>
-                            
-                        
-                        </td>
-                        
-                        
-                    
-                    <!-- prev syllabus -->
-                    <!-- 
-                    
- 					-->
-                        
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td><img src='/src08/images/common/ico_check5.gif'></td>
-                    <td>이평래
-                        
-                            <br>(Lee  pyungrae)
-                        </td>
-                    <td>2</td>
-                    <td>2</td>
-                    <td>월 7 8 (1404)<br>(Mon 7 8 (1404))</td>
-                    <td>16&nbsp;/&nbsp;50</td>
-                    <td class="align_left">러시아.중앙아시아지역<br>
-                        
-                            <font class="txt_navy">
-                        </td>
-                </tr>
-     */
+console.log(data)
+module.exports = {getData, parseGetLeftSeat, noticeEmpty}
