7baa6332b7cad0df0357907a3029f9c8746840b8
eatnug <wlq4568@gmail.com>
2019-09-23 22:28:55 +0900

    added modal opener & closer & button

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,70 @@
 import React from "react";
 import axios from "axios";
+import Course from "./Course";
+import Find from "./Find";
+import Modal from "./Modal"
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      loaded: false
+      isModal: false,
+      loaded: false,
+      courses: [
+        {
+          grade: 1,
+          number: "a1234",
+          title: "first",
+          professor: "eatnug",
+          time: "월 456",
+          classroom: "3203",
+          apply: "10/10"
+        },
+        {
+          grade: 1,
+          number: "b5678",
+          title: "second",
+          professor: "eatnug",
+          time: "화 123",
+          classroom: "0013",
+          apply: "20/20"
+        }
+      ]
     };
+    this.modalOpener = this.openModal.bind(this);
+    this.modalCloser = this.closeModal.bind(this);
   }
-  componentDidMount = async () => {
-    const { data } = await axios.get("http://127.0.0.1:4000/test");
-    this.setState({ loaded: true, data });
-  };
+
+  openModal = () => this.setState({ isModal: true });
+  closeModal = () => this.setState({ isModal: false });
 
   render = () => {
-    const { loaded, data } = this.state;
-    if (!loaded) return <div>loading ...</div>;
-    else return <div>{data}</div>;
+    const {courses, isModal} = this.state
+    return (
+      <div className="container">
+        <div className="courses">
+          {courses.map(
+            (
+              { grade, number, title, professor, time, classroom, apply },
+              ind
+            ) => (
+              <Course
+                key={ind}
+                grade={grade}
+                number={number}
+                title={title}
+                professor={professor}
+                time={time}
+                classroom={classroom}
+                apply={apply}
+              />
+            )
+          )}
+        </div>
+        <Find modalOpener={this.modalOpener} />
+        {isModal ? <Modal modalCloser={this.modalCloser}/> : null}
+      </div>
+    );
   };
 }
 
