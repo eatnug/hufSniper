f01ca7f6220d48d60ed6edcb72ed476662e43aa1
eatnug <wlq4568@gmail.com>
2019-09-24 10:28:25 +0900

    getting data from api server with qs

--- client/src/Modal.js
+++ client/src/Modal.js
@@ -1,31 +1,21 @@
 import React from "react";
-import "./Modal.css";
+import Form from "./Form";
 class Modal extends React.Component {
-    constructor(props){
-        super(props)
-        this.changeHandler = this.handleChange.bind(this)
-        this.state = {
-            tab_lang:"K",
-            ag_ledg_year: "",
-            ag_ledg_sessn:"",
-            ag_org_sect:"",
-            campus_sect:"",
-            ag_crs_strct_cd:"",
-            ag_compt_fld_cd:"",
-            course_number:"",
-        }
-    }
-    handleChange = (e) => {
-        this.setState({[e.target.id]:e.target.value})
-    }
+  constructor(props) {
+    super(props);
+  }
+  handleChange = e => {
+    this.setState({ [e.target.id]: e.target.value });
+  };
   render() {
     const { modalCloser } = this.props;
     return (
       <div className="modal">
         <div className="form__container">
-            {Object.keys(this.state).slice(1).map(c=>(<label key={c}>
-                {c}<input id={c}type="text" value={this.state[c]} onChange={this.changeHandler}></input><br></br>
-            </label>))}
+          <Form />
+        </div>
+        <div className="courses">
+          courses
         </div>
         <button className="modal__closer" onClick={modalCloser}>
           Close
