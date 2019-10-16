888f5c5446eea8cb443c3e3a7c0c34e24ab89eb5
eatnug <wlq4568@gmail.com>
2019-10-16 13:08:18 +0900

    yarn add redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './component/App';
-import {createStore} from 'react-redux'
+import {createStore} from 'react'
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
