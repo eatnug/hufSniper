48fccf57198f4169019aebe747102dddd51b9a25
eatnug <wlq4568@gmail.com>
2019-10-30 20:48:19 +0900

    add parseClass

--- server/api.js
+++ server/api.js
@@ -1,7 +1,6 @@
-const axios = require('axios')
-const qs = require('qs')
-const cheerio = require('cheerio');
-
+const axios = require("axios");
+const qs = require("qs");
+const cheerio = require("cheerio");
 
 /**
  *  axios post로 데이터 받아오는 함수
@@ -9,7 +8,7 @@ const cheerio = require('cheerio');
  * @param {*} data 포스트로 넘길 폼 데이터 qs stringify
  */
 async function getData(data) {
-    return await axios.post("https://wis.hufs.ac.kr/src08/jsp/lecture/LECTURE2020L.jsp", data, {'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'});
+  return await axios.post("https://wis.hufs.ac.kr/src08/jsp/lecture/LECTURE2020L.jsp", data, { "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8" });
 }
 
 /**
@@ -18,22 +17,43 @@ async function getData(data) {
  * @param {*} CN course number
  */
 const parseGetLeftSeat = async (res, CN) => {
-    return new Promise((resolve, reject) => {
-        const $ = cheerio.load(res.data);
-        $("#premier1 > div > table > tbody > tr > td").each((i, e) => {
-            if (e.firstChild && e.firstChild.data == CN && e.next) {
-                const tmp = e.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.firstChild.data.split('/')
-                resolve(Number(tmp[1].trim()) - Number(tmp[0].trim()) < 1 ? false : true)
-            }
-        })
-    })
-}
+  return new Promise((resolve, reject) => {
+    const $ = cheerio.load(res.data);
+    $("#premier1 > div > table > tbody > tr > td").each((i, e) => {
+      if (e.firstChild && e.firstChild.data == CN && e.next) {
+        const tmp = e.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.next.firstChild.data.split("/");
+        resolve(Number(tmp[1].trim()) - Number(tmp[0].trim()) < 1 ? false : true);
+      }
+    });
+  });
+};
 
-const scanEmpty = async(data, CN, cb) => {
-    if(await parseGetLeftSeat(await getData(data), CN)) cb()
-    else {
-        await scanEmpty(data,CN,cb)
+const scanEmpty = async (data, CN, cb) => {
+  if (await parseGetLeftSeat(await getData(data), CN)) cb();
+  else {
+    await scanEmpty(data, CN, cb);
+  }
+};
+
+const parseClass = html => {
+  const $ = cheerio.load(html);
+  const target = $("#premier1 tbody tr").text();
+  const courses = target.replace(/\n\s+\n\s+((\d{1,2})\n\s+\D{2,})/gi, "$ $1").split("$");
+  courses.shift();
+  return courses
+  .map((cur, ind) => {
+    if (ind % 2 == 0) return (cur + courses[ind + 1]).replace(/[\s\n\t]{2,}([\S])/gi, "$ $1").split("$");
+  })
+  .filter((cur, ind) => ind % 2 == 0).map(cur => {
+    return {
+      title: `${cur[4]}/${cur[5]}`,
+      grade: cur[2],
+      courseNumber: cur[3],
+      professor: cur[6],
+      timePlace: cur[9],
+      apply: cur[10],
     }
-}
+  })
+};
 
-module.exports = {getData, parseGetLeftSeat, scanEmpty}
+module.exports = { getData, parseGetLeftSeat, scanEmpty, parseClass };
--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ var cors = require("cors");
 const qs = require("qs");
 const PORT = process.env.PORT || 4000;
 
-const { getData, scanEmpty } = require("./api");
+const { parseClass, getData, scanEmpty } = require("./api");
 
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
@@ -27,30 +27,7 @@ app.get("/", (req, res) => {
 
 app.post("/getList", async (req, res) => {
   const html = await getData(qs.stringify(req.body));
-  const target = html.data.split(`<th>비고<br>(Note)</th>`)[1];
-  const re_tr = /<tr([\s\S]*?)<\/tr>/g;
-  const re_td = /<td[\s\S]*?>([\s\S]*?)<\/td>/g;
-  const re_title = /<font class="txt_navy">([\s\S]*?)<br>/g;
-  const array = [];
-  while ((tmp = re_tr.exec(target))) {
-    array.push(tmp[1]);
-  }
-  const objs = array.map(c => {
-    const obj = {};
-    const attr = [];
-    while ((_tmp = re_td.exec(c))) {
-      attr.push(_tmp);
-    }
-    obj.title = re_title.exec(attr[4][1])[1];
-    obj.grade = attr[2][1];
-    obj.courseNumber = attr[3][1];
-    obj.professor = attr[11][1].split("<br>")[0].trim();
-    obj.timePlace = attr[14][1].split("<br>")[0];
-    obj.apply = attr[15][1].replace(/&nbsp;/gi, "");
-    console.log(`${re_title.exec(attr[4][1])}`);
-    return obj;
-  });
-  res.send(objs);
+  res.send(parseClass(html.data))
 });
 
 app.post("/getLeftSeat", async (req, res) => {
