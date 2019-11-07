455f937e5c6b1b6631adeab94642979a045df4dd
eatnug <wlq4568@gmail.com>
2019-11-07 16:34:03 +0900

    added className

--- client/src/Course.js
+++ client/src/Course.js
@@ -6,31 +6,37 @@ import qs from "qs";
 const Course = ({ ctx, course, where }) => {
   const dispatch = useDispatch();
   return (
-    <div className="course">
+    <div className="course__container">
       <div className="course__detail">{`${course.title} //${course.professor}// ${course.apply}`}</div>
       {where === "form" ? (
-        <button
-          onClick={() => {
-            axios.post("http://127.0.0.1:3000/api/getLeftSeat", qs.stringify({ ...ctx, index: course.index })).then(res => {
-              alert(res.data.code);
-              dispatch(remCourse(course.courseNumber));
-            });
-            return dispatch(addCourse({info:course,ctx}));
-          }}
-        >
-          add
-        </button>
+        <div className="button__container">
+          <button
+            className="button__self"
+            onClick={() => {
+              axios.post("http://127.0.0.1:3000/api/getLeftSeat", qs.stringify({ ...ctx, index: course.index })).then(res => {
+                alert(res.data.code);
+                dispatch(remCourse(course.courseNumber));
+              });
+              return dispatch(addCourse({ info: course, ctx }));
+            }}
+          >
+            add
+          </button>
+        </div>
       ) : (
-        <button
-          onClick={() => {
-            axios.post("http://127.0.0.1:3000/api/stopTracking", qs.stringify({ ...ctx, index: course.index })).then(res => {
-              alert(res.data.code);
-            });
-            return dispatch(remCourse(course.courseNumber));
-          }}
-        >
-          rem
-        </button>
+        <div className="button__container">
+          <button
+            className="button__self"
+            onClick={() => {
+              axios.post("http://127.0.0.1:3000/api/stopTracking", qs.stringify({ ...ctx, index: course.index })).then(res => {
+                alert(res.data.code);
+              });
+              return dispatch(remCourse(course.courseNumber));
+            }}
+          >
+            rem
+          </button>
+        </div>
       )}
     </div>
   );
